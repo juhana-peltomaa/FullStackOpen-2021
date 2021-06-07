f3f003c30e94fc592e828b316d646ff2f3adea7c
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-06-07 21:30:15 +0200

    Exercise 2.10

---
+++ part2/phonebook/src/components/PersonForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PersonForm = (props) => {
+    return(
+      <form onSubmit={props.setToNewName}>
+        <div>
+          name: <input value={props.newName} onChange={props.handleNewName} />
+        </div>
+        <div>
+          number: <input value={props.newNumber} onChange={props.handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+    </form>
+    )
+  }
+
+  export default PersonForm
