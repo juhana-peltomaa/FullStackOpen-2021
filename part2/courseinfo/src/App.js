e38ad52137b77353ec93d179b517c6fe858bcb98
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-06-07 16:34:34 +0200

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,49 +1,5 @@
 import React from 'react'
-
-const Header = (props) => {
-  return(
-    <div>
-      <h2>{props.course.name}</h2>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return(
-      <p>{props.part} {props.exercises}</p>
-  )
-}
-
-const Content = (props) => {
-
-  return(
-    <div>
-      {props.part.map(part => <Part key={part.id} part={part.name} exercises={part.exercises}/>)}
-    </div>
-  )
-}
-
-const Total = props => {
-
-  const total = props.part.parts.reduce(
-    (sum, amount) => sum + amount.exercises,
-    0
-  );
-  return <p><strong> total of {total} exercises </strong></p>;
-};
-
-
-const Course = ({ course }) => {
-  return (
-  <div>
-    <Header key={course.id} course={course} />
-    <Content part={course.parts}/>
-    <Total part={course}/>
-  </div>
-
-  )
-}
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -95,7 +51,9 @@ const App = () => {
     <div>
       <h1>Web development curriculum</h1>
       {courses.map(course =>
-    <Course course={course} />
+      <div key={course.id}>
+        <Course course={course} />
+      </div>
       )}
     </div>
     )
