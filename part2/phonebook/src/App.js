249f67bd3602ae6044ed06270b26efb9db6aae18
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-06-08 15:53:43 +0200

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Person from './components/Person'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
@@ -18,6 +14,19 @@ const App = () => {
   const [ filtteri, setFilter ] = useState('')
 
   
+  useEffect(() => {
+    console.log("effect here")
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log("promise here")
+      setPersons(response.data)
+      console.log(persons)
+  })
+}, [])
+console.log("new persons", persons.length)
+
+
   const setToNewName = (e) => {
     e.preventDefault()
 
